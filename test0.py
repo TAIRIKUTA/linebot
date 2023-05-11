640e0ff06798a73dad1c5a26e6dc4e5fc55552b9
TAIRIKUTA <49270004+TAIRIKUTA@users.noreply.github.com>
2023-05-11 18:32:01 +0900

    Update test0.py

--- test0.py
+++ test0.py
@@ -1,36 +1,51 @@
-from fastapi import FastAPI, Request
-from linebot import WebhookParser, LineBotApi
-from linebot.models import TextSendMessage
+from flask import Flask, request, abort
 
-LINE_CHANNEL_ACCESS_TOKEN = 'dV3KYxrI2IA21lg6wwUOr9Gm9Jw8xVxlrt2/HO479tuI5t9ALkjt4KhS627+KOLmePUSBwMQYbZDGbHRrFjOTMrF/WyBhY2xJog321Pb3EWwd4FoHGlg0ZPzWrXtQq0O/MxleYUT7A+T2ccyYMpg9QdB04t89/1O/w1cDnyilFU='
-LINE_CHANNEL_SECRET = '166564241d6d37868b6d043b6166a0dc'
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
+
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-line_parser = WebhookParser(LINE_CHANNEL_SECRET)
-app = FastAPI()
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
-@app.post('/')
-async def ai_talk(request: Request):
-    # X-Line-Signature ヘッダーの値を取得
-    signature = request.headers.get('X-Line-Signature', '')
-
-    # request body から event オブジェクトを取得
-    events = line_parser.parse((await request.body()).decode('utf-8'), signature)
-
-    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェっｚクトが含まれる場合あるため）
-    for event in events:
-        if event.type != 'message':
-            continue
-        if event.message.type != 'text':
-            continue
-
-        # LINE パラメータの取得
-        line_user_id = event.source.user_id
-        line_message = event.message.text
-
-        print(line_message)
-        line_bot_api.push_message(line_user_id,TextSendMessage(text='Hello World!'))
-    #LINE Webhook サーバーへ HTTP レスポンスを返す
-    return 'ok'
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
