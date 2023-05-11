1ed4b837d39877fedfbc0c51d2d427f732093687
TAIRIKUTA <49270004+TAIRIKUTA@users.noreply.github.com>
2023-05-11 19:01:18 +0900

    Update test0.py

--- test0.py
+++ test0.py
@@ -39,10 +39,10 @@ def callback():
 
 # handle message from LINE
 @handler.add(MessageEvent, message=TextMessage)
-    def handle_message(event):
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=event.message.text))
+def handle_message(event):
+　　 line_bot_api.reply_message(
+	event.reply_token,
+	TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
 	app.run()
